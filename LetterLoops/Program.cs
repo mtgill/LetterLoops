689859eae25fbcbf7c516c530bd6b005af580d95
Matt Gill <mtgill90@outlook.com>
2019-08-21 22:24:33 -0500

    letters do the looping

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           List<string> letterList = new List<string>();
+            Console.WriteLine("Please enter a group of letters");
+            var inputLetters = Console.ReadLine();
+
+            List<string> singleLetterList = new List<string>();
+            for (var i = 0; i < inputLetters.Length; i++)
+            {
+                singleLetterList.Add(inputLetters[i].ToString());
+   
+                int numCount = i + 1;
+                string singleLetter = inputLetters[i].ToString().ToLower();
+                for (var k = 0; k < numCount; k++)
+                {
+                    Console.Write((k == 0 ? singleLetter.ToUpper() : singleLetter));
+                }
+                Console.Write("-");
+            }
+        }
+    }
+}
